e1202e210208c26371a2037c82631086f20fa8fc
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2015-09-11 21:42:27 +0200

    Add DATADIR to parallel.go

--- parallel-copy/parallel.go
+++ parallel-copy/parallel.go
@@ -7,12 +7,13 @@ import (
 	"sync"
 )
 
-func run_node(cmd_str string, node int, origin int, wg *sync.WaitGroup) {
+func run_node(wg *sync.WaitGroup, cmd_str string, node int, origin int, data_dir string) {
 	cmd := exec.Command("sh", "-c", cmd_str)
-        cmd.Stderr = os.Stderr
-        cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
 	cmd.Env = append(os.Environ(), fmt.Sprintf("NODE=ccqserv%d", node))
 	cmd.Env = append(cmd.Env, fmt.Sprintf("ORIGIN=ccqserv%d", origin))
+	cmd.Env = append(cmd.Env, fmt.Sprintf("DATADIR=%v", data_dir))
 	fmt.Printf("command is: %s\n", cmd_str)
 	err := cmd.Run()
 	if err != nil {
@@ -22,23 +23,24 @@ func run_node(cmd_str string, node int, origin int, wg *sync.WaitGroup) {
 }
 
 func main() {
-	const nb_node = 25 
+	const nb_node = 25
 	const first_node = 100
-	const first_node_new = 125 
+	const first_node_new = 125
+	const data_dir = "/qserv/data"
 	wg := new(sync.WaitGroup)
 
 	cmd_str_list := []string{
-		`ssh -K ${ORIGIN} "sudo -u qserv sh -c 'chmod -R o+rx /qserv/data'"`,
-		`ssh -K ${NODE} "sudo -u qserv sh -c 'mkdir -p /qserv/data && chmod -R o+w /qserv/data || echo \"Unable to chown /qserv/data\"'"`,
-		`ssh -K ${NODE} "rsync -e 'ssh -K' --delete -r ${USER}@${ORIGIN}.in2p3.fr:/qserv/data /qserv/"`,
-		`ssh -K ${NODE} "sudo sh -c 'chown qserv:qserv /qserv/data'"`,
-		`ssh -K ${NODE} "sudo -u qserv sh -c 'chmod -R o-w /qserv'"`,
+		`ssh -K ${ORIGIN} "sudo -u qserv sh -c 'chmod -R o+rx ${DATADIR}'"`,
+		`ssh -K ${NODE} "sudo -u qserv sh -c \"mkdir -p ${DATADIR} && chmod -R o+w ${DATADIR} || echo 'Unable to chown '${DATADIR}\""`,
+		`ssh -K ${NODE} "rsync -e 'ssh -K' --delete -r ${USER}@${ORIGIN}.in2p3.fr:${DATADIR} /qserv/"`,
+		`ssh -K ${NODE} "sudo sh -c \"chown -R qserv:qserv ${DATADIR}\""`,
+		`ssh -K ${NODE} "sudo -u qserv sh -c \"chmod -R o-w ${DATADIR}\""`,
 	}
 
 	for j := 0; j < len(cmd_str_list); j++ {
 		wg.Add(nb_node)
 		for i := first_node_new; i < first_node_new+nb_node; i++ {
-			go run_node(cmd_str_list[j], i, i-nb_node, wg)
+			go run_node(wg, cmd_str_list[j], i, i-nb_node, data_dir)
 		}
 		wg.Wait()
 	}
