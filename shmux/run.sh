62a30fc2e95e4f9ae5278250303ffee078138cc8
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2015-09-21 12:42:13 +0200

    Fix port option for container execution

--- shmux/run.sh
+++ shmux/run.sh
@@ -1,9 +1,9 @@
 /opt/shmux/bin/shmux -c "sudo -u qserv sh -c 'docker rm -f qserv; nohup docker run --name qserv --rm -h ccqserv125.in2p3.fr \
     -p 4040:4040 -p 1094:1094 -p 2131:2131 -p 12181:12181 -p 5012:5012 \
-    -v /qserv/data:/qserv/data -P fjammes/qserv:master-ccqserv125.in2p3.fr \
+    -v /qserv/data:/qserv/data fjammes/qserv:master-ccqserv125.in2p3.fr \
     > qserv.out 2> qserv.err < /dev/null &'" ccqserv125.in2p3.fr
 
 /opt/shmux/bin/shmux -c "sudo -u qserv sh -c 'docker rm -f qserv; nohup docker run --name qserv --rm -h worker-docker \
     -p 1094:1094 -p 5012:5012 \
-    -v /qserv/data:/qserv/data -P fjammes/qserv:worker-ccqserv125.in2p3.fr \
+    -v /qserv/data:/qserv/data fjammes/qserv:worker-ccqserv125.in2p3.fr \
     > qserv.out 2> qserv.err < /dev/null &'" ccqserv{126..149}.in2p3.fr 
