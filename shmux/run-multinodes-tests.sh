d4225e133ebff97a73b18e72fd3c29e06059a230
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2015-09-23 23:01:18 +0200

    Manage hostname for docker container

--- shmux/run-multinodes-tests.sh
+++ shmux/run-multinodes-tests.sh
@@ -5,7 +5,7 @@ set -x
     fjammes/qserv:master-ccqserv125.in2p3.fr \
     > qserv.out 2> qserv.err < /dev/null &'" ccqserv125.in2p3.fr
 
-/opt/shmux/bin/shmux -c "sudo -u qserv sh -c 'docker rm -f qserv; nohup docker run --name qserv --rm -h $(hostname --fqdn) \
+/opt/shmux/bin/shmux -c 'sudo -u qserv sh -c "docker rm -f qserv; nohup docker run --name qserv --rm -h $(hostname --fqdn) \
     -p 1094:1094 -p 5012:5012 \
     fjammes/qserv:worker-ccqserv125.in2p3.fr \
-    > qserv.out 2> qserv.err < /dev/null &'" ccqserv{126..127}.in2p3.fr
+    > qserv.out 2> qserv.err < /dev/null &"' ccqserv{126..127}.in2p3.fr
--- shmux/run.sh
+++ shmux/run.sh
@@ -3,7 +3,7 @@
     -v /qserv/data:/qserv/data fjammes/qserv:master-ccqserv125.in2p3.fr \
     > qserv.out 2> qserv.err < /dev/null &'" ccqserv125.in2p3.fr
 
-/opt/shmux/bin/shmux -c "sudo -u qserv sh -c 'docker rm -f qserv; nohup docker run --name qserv --rm -h worker-docker \
+/opt/shmux/bin/shmux -c 'sudo -u qserv sh -c "docker rm -f qserv; nohup docker run --name qserv --rm -h $(hostname --fqdn) \
     -p 1094:1094 -p 5012:5012 \
     -v /qserv/data:/qserv/data fjammes/qserv:worker-ccqserv125.in2p3.fr \
-    > qserv.out 2> qserv.err < /dev/null &'" ccqserv{126..149}.in2p3.fr 
+    > qserv.out 2> qserv.err < /dev/null &"' ccqserv{126..149}.in2p3.fr 
