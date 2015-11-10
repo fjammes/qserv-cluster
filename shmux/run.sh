83917f137b6323840e629cf631fa2a87a02b90b5
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2015-11-09 17:08:15 -0800

    Deploy user images

--- shmux/run.sh
+++ shmux/run.sh
@@ -1,9 +1,19 @@
-/opt/shmux/bin/shmux -c "sudo -u qserv sh -c 'docker rm -f qserv; nohup docker run --name qserv --rm -h ccqserv125.in2p3.fr \
-    -p 4040:4040 -p 1094:1094 -p 2131:2131 -p 12181:12181 -p 5012:5012 \
-    -v /qserv/data:/qserv/data fjammes/qserv:master-ccqserv125.in2p3.fr \
-    > qserv.out 2> qserv.err < /dev/null &'" ccqserv125.in2p3.fr
+# PRE-REQUISITE: run 'changesh bash' on ccage
+set -e
+set -x
 
-/opt/shmux/bin/shmux -c 'sudo -u qserv sh -c "docker rm -f qserv; nohup docker run --name qserv --rm -h $(hostname --fqdn) \
+DOCKER_USER=jgates108
+MASTER=ccqserv125.in2p3.fr
+BRANCH=tickets_DM-3945
+MASTER_IMAGE="$DOCKER_USER/qserv:${BRANCH}_master_$MASTER"
+WORKER_IMAGE="$DOCKER_USER/qserv:${BRANCH}_worker_$MASTER"
+
+/opt/shmux/bin/shmux -c "sudo -u qserv bash -c 'docker rm -f qserv; nohup docker run --name qserv --rm -h ccqserv125.in2p3.fr \
+    -p 4040:4040 -p 1094:1094 -p 2131:2131 -p 12181:12181 -p 5012:5012 \
+    -v /qserv/data:/qserv/data $MASTER_IMAGE \
+    > /tmp/qserv.out 2> /tmp/qserv.err < /dev/null &'" ccqserv125.in2p3.fr
+
+/opt/shmux/bin/shmux -c 'sudo -u qserv bash -c "docker rm -f qserv; nohup docker run --name qserv --rm -h $(hostname --fqdn) \
     -p 1094:1094 -p 5012:5012 \
-    -v /qserv/data:/qserv/data fjammes/qserv:worker-ccqserv125.in2p3.fr \
-    > qserv.out 2> qserv.err < /dev/null &"' ccqserv{126..149}.in2p3.fr 
+    -v /qserv/data:/qserv/data '$WORKER_IMAGE' \
+    > /tmp/qserv.out 2> /tmp/qserv.err < /dev/null &"' ccqserv{126..149}.in2p3.fr
