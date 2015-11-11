67fccd926d4ffb776335d37a68a296f4da4f70fd
John Gates <jgates@slac.stanford.edu>
2015-11-11 22:44:43 +0100

    Cluster fixes

--- shmux/run.sh
+++ shmux/run.sh
@@ -1,14 +1,21 @@
+set -e
+set -x
+
 DOCKER_USER=jgates108
 MASTER=ccqserv125.in2p3.fr
-MASTER_IMAGE=$DOCKER_USER/qserv:master-$MASTER
-WORKER_IMAGE=$DOCKER_USER/qserv:worker-$MASTER
+BRANCH=tickets_DM-3945
+MASTER_IMAGE="$DOCKER_USER/qserv:${BRANCH}_master_$MASTER"
+WORKER_IMAGE="$DOCKER_USER/qserv:${BRANCH}_worker_$MASTER"
 
-/opt/shmux/bin/shmux -c "sudo -u qserv sh -c 'docker rm -f qserv; nohup docker run --name qserv --rm -h ccqserv125.in2p3.fr \
+#MASTER_IMAGE=fjammes/qserv:master-ccqserv125.in2p3.fr
+#WORKER_IMAGE=fjammes/qserv:worker-ccqserv125.in2p3.fr
+
+/opt/shmux/bin/shmux -c "sudo -u qserv bash -c 'docker rm -f qserv; nohup docker run --name qserv --rm -h ccqserv125.in2p3.fr \
     -p 4040:4040 -p 1094:1094 -p 2131:2131 -p 12181:12181 -p 5012:5012 \
     -v /qserv/data:/qserv/data $MASTER_IMAGE \
-    > qserv.out 2> qserv.err < /dev/null &'" ccqserv125.in2p3.fr
+    > /tmp/qserv.out 2> /tmp/qserv.err < /dev/null &'" ccqserv125.in2p3.fr
 
-/opt/shmux/bin/shmux -c 'sudo -u qserv sh -c "docker rm -f qserv; nohup docker run --name qserv --rm -h $(hostname --fqdn) \
+/opt/shmux/bin/shmux -c 'sudo -u qserv bash -c "docker rm -f qserv; nohup docker run --name qserv --rm -h $(hostname --fqdn) \
     -p 1094:1094 -p 5012:5012 \
-    -v /qserv/data:/qserv/data $WORKER_IMAGE \
-    > qserv.out 2> qserv.err < /dev/null &"' ccqserv{126..149}.in2p3.fr 
+    -v /qserv/data:/qserv/data '$WORKER_IMAGE' \
+    > /tmp/qserv.out 2> /tmp/qserv.err < /dev/null &"' ccqserv{126..149}.in2p3.fr 
