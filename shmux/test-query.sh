12efe60b18ba563634874f1a7a942f01c079b3a8
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2016-02-18 23:15:37 +0100

    Add few utilities

--- shmux/test-query.sh
+++ shmux/test-query.sh
@@ -1,3 +1,5 @@
 . /qserv/stack/loadLSST.bash 
 setup mysqlclient
 time mysql --host ccqserv125 --port 4040 --user qsmaster LSST -e "SELECT ra, decl FROM Object WHERE deepSourceId = 2322920177142607;";
+setup mysqlpython
+python runQueries.py
