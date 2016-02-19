94c2b0b8d1f8048d5214b774edeb1723a12b95e3
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2016-02-19 20:25:27 +0100

    Write logfiles on host

--- shmux/run.sh
+++ shmux/run.sh
@@ -7,12 +7,14 @@ set -x
 /opt/shmux/bin/shmux -c "docker rm -f $CONTAINER_NAME; \
         docker run --detach=true \
             -v /qserv/data:/qserv/data \
+            -v /qserv/log:/qserv/run/var/log \
             --name $CONTAINER_NAME --net=host \
                 $MASTER_IMAGE" ccqserv125.in2p3.fr
 
-/opt/shmux/bin/shmux -c "docker rm -f $CONTAINER_NAME; \
+/opt/shmux/bin/shmux -S all -c "docker rm -f $CONTAINER_NAME; \
         docker run --detach=true \
             -v /qserv/data:/qserv/data \
+            -v /qserv/log:/qserv/run/var/log \
             --name $CONTAINER_NAME --net=host \
-                $WORKER_IMAGE" $WORKERS ccqserv{126..149}.in2p3.fr
+                $WORKER_IMAGE" ccqserv{126..149}.in2p3.fr
 
